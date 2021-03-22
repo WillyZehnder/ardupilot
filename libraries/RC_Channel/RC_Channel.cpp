32e9a15422c75ecdfd9107b1e18f277dfed4932d
WillyZehnder <willy.mint@gmx.de>
2021-03-22 10:46:51 +0100

    RC_Channel: a few lines to prevent "unknown" warning
    RC_Channel: MISSION_RELATIVE_ENABLED

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -495,6 +495,7 @@ void RC_Channel::init_aux_function(const aux_func_t ch_option, const AuxSwitchPo
     case AUX_FUNC::EKF_YAW_RESET:
     case AUX_FUNC::GENERATOR: // don't turn generator on or off initially
     case AUX_FUNC::EKF_POS_SOURCE:
+    case AUX_FUNC::RELOCATE_MISSION:
     case AUX_FUNC::SCRIPTING_1:
     case AUX_FUNC::SCRIPTING_2:
     case AUX_FUNC::SCRIPTING_3:
@@ -577,6 +578,9 @@ const RC_Channel::LookupTable RC_Channel::lookuptable[] = {
     { AUX_FUNC::CAM_MODE_TOGGLE,"CamModeToggle"},
     { AUX_FUNC::GENERATOR,"Generator"},
     { AUX_FUNC::ARSPD_CALIBRATE,"Calibrate Airspeed"},
+#ifdef MISSION_RELOCATE_ENABLED
+    { AUX_FUNC::RELOCATE_MISSION,"Relocate Mission"},
+#endif
 };
 
 /* lookup the announcement for switch change */
@@ -1176,6 +1180,8 @@ bool RC_Channel::do_aux_function(const aux_func_t ch_option, const AuxSwitchPos 
         // used to test emergency yaw reset
         AP::ahrs().request_yaw_reset();
         break;
+
+    case AUX_FUNC::RELOCATE_MISSION:
 
     case AUX_FUNC::SCRIPTING_1:
     case AUX_FUNC::SCRIPTING_2:
